1467a72e905bc188f1f534cb3adeda164b565a28
Teddy Engel <engel.teddy@gmail.com>
2018-04-28 16:44:21 +0100

    Add new abstract system types

---
+++ SingletonSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChocolateECS
+{
+    public abstract class SingletonSystem : ISystem
+    {
+        public virtual void OnAwake(List<ISystem> systems)
+        {
+        }
+
+        public virtual void OnStart()
+        {
+            
+        }
+
+        public virtual void OnEnable()
+        {
+            
+        }
+
+        public virtual void OnUpdate(float deltaTime)
+        {
+
+        }
+
+        public virtual void OnFixedUpdate()
+        {
+
+        }
+
+        public virtual void OnDisable()
+        {
+
+        }
+
+        public virtual void OnDestroy()
+        {
+
+        }        
+    }
+}
