fd49792abb14b19d378afabbad0dce823f5e32a2
Teddy Engel <engel.teddy@gmail.com>
2018-05-05 15:43:28 +0100

    Make component manager only add / remove relevant component, not all of them

--- ComponentManager.cs
+++ ComponentManager.cs
@@ -9,38 +9,28 @@ namespace ChocolateECS
         List<Component> _emptyList = new List<Component>(); // Just used for empty returns
         Dictionary<Type, List<Component>> _components = new Dictionary<Type, List<Component>>();
         Dictionary<Type, Dictionary<Type, List<Component>>> _secondaryComponents = new Dictionary<Type, Dictionary<Type, List<Component>>>();
+        List<Type> _types = new List<Type>();
 
         public void OnGameObjectInstantiated(GameObject gameObject)
         {
-            Debug.Log("ComponentManager::OnGameObjectInstantiated");
-            // TODO: Eventually, only add the game object to the list of _components that match
-            // RefreshComponents();
-            RefreshComponents();
+            AddGameObjectMainComponents(gameObject);
+            AddGameObjectSecondaryComponents(gameObject);
         }
 
         public void OnGameObjectPreDestroyed(GameObject gameObject)
         {
-            Debug.Log("ComponentManager::OnGameObjectPreDestroyed");
-            // Debug.Log("Before: " + countMainComponents);
-            // for (int i = 0; i < countMainComponents; ++i)
-            // {
-            //     Debug.Log(mainComponents[i].gameObject.GetInstanceID());
-            // }
-            // RemoveGameObjectsFromMainComponents(gameObject);
-            // Debug.Log("After: " + countMainComponents);
-            // for (int i = 0; i < countMainComponents; ++i)
-            // {
-            //     Debug.Log(mainComponents[i].gameObject.GetInstanceID());
-            // }
-            // RefreshMainComponents();
+            var gameObjectComponents = gameObject.GetComponents(typeof(Component));
+
+            for (int i = 0; i < gameObjectComponents.Length; ++i)
+            {
+                for (int j = 0; j < _types.Count; ++j)
+                    RemoveSecondaryComponent(_types[j], gameObjectComponents[i]);
+                RemoveMainComponent(gameObjectComponents[i]);
+            }
         }
 
         public void OnGameObjectPostDestroyed(GameObject gameObject)
         {
-            Debug.Log("ComponentManager::OnGameObjectPostDestroyed");
-            // TODO: Eventually, only refresh _components with the same type as gameObject
-            // RefreshComponents();
-            RefreshComponents();
         }
 
         void RefreshComponents()
@@ -55,54 +45,82 @@ namespace ChocolateECS
             // Create the main lists of _components
             for (int i = 0; i < foundObjects.Length; ++i)
             {
-                var gameObjectComponents = foundObjects[i].GetComponents(typeof(Component));
+                AddGameObjectMainComponents(foundObjects[i]);
+                AddGameObjectSecondaryComponents(foundObjects[i]);
+            }
+        }
+
+        void AddGameObjectMainComponents(GameObject gameObject)
+        {
+            var gameObjectComponents = gameObject.GetComponents(typeof(Component));
+
+            for (int i = 0; i < gameObjectComponents.Length; ++i)
+                AddMainComponent(gameObjectComponents[i]);
+        }
+
+        void AddMainComponent(Component mainComponent)
+        {
+            Type mainComponentType = mainComponent.GetType();
+
+            if (!_components.ContainsKey(mainComponentType))
+            {
+                _components.Add(mainComponentType, new List<Component>());
+                if (!_types.Contains(mainComponentType))
+                    _types.Add(mainComponentType);
+            }
+            _components[mainComponentType].Add(mainComponent);
+        }
+
+        void RemoveMainComponent(Component mainComponent)
+        {
+            Type mainComponentType = mainComponent.GetType();
+
+            if (!_components.ContainsKey(mainComponentType))
+                return;
+            _components[mainComponentType].Remove(mainComponent);
+            if (_components[mainComponentType].Count == 0)
+            {
+                _components.Remove(mainComponentType);
+                _types.Remove(mainComponentType);
+            }
+        }
+
+        void AddGameObjectSecondaryComponents(GameObject gameObject)
+        {
+            var gameObjectComponents = gameObject.GetComponents(typeof(Component));
+
+            for (int i = 0; i < _types.Count; ++i)
+            {
+                Type mainComponentType = _types[i];
 
                 for (int j = 0; j < gameObjectComponents.Length; ++j)
-                {
-                    Type componentType = gameObjectComponents[j].GetType();
-
-                    if (!_components.ContainsKey(componentType))
-                    {
-                        _components.Add(componentType, new List<Component>()); 
-                        _secondaryComponents.Add(componentType, new Dictionary<Type, List<Component>>()); 
-                    }
-                    _components[componentType].Add(gameObjectComponents[j]);
-                }
+                    AddSecondaryComponent(mainComponentType, gameObjectComponents[j]);
             }
+        }
 
-            // Go through main component types
-            foreach (KeyValuePair<Type, List<Component>> entry in _components)
-            {
-                Type mainType = entry.Key;
+        void AddSecondaryComponent(Type mainComponentType, Component secondaryComponent)
+        {
+            Type secondaryComponentType = secondaryComponent.GetType();
 
-                //  For each type, go through _components
-                foreach (Component mainComponent in entry.Value)
-                {
-                    // For each component, try and get all possible component type
-                    foreach (Type secondaryType in _components.Keys)
-                    {
-                        if (secondaryType == mainType)
-                            continue;
-                        Component secondaryComponent = mainComponent.gameObject.GetComponent(secondaryType);
-                        if (secondaryComponent == null)
-                            continue;
-                        if (!_secondaryComponents[mainType].ContainsKey(secondaryType))
-                            _secondaryComponents[mainType].Add(secondaryType, new List<Component>());
-                        _secondaryComponents[mainType][secondaryType].Add(secondaryComponent);
-                    }
-                }
-            }
+            if (!_secondaryComponents.ContainsKey(mainComponentType))
+                _secondaryComponents.Add(mainComponentType, new Dictionary<Type, List<Component>>());
+            if (!_secondaryComponents[mainComponentType].ContainsKey(secondaryComponentType))
+                _secondaryComponents[mainComponentType].Add(secondaryComponentType, new List<Component>());
+            _secondaryComponents[mainComponentType][secondaryComponentType].Add(secondaryComponent);
+        }
 
-            /*
-            foreach (KeyValuePair<Type, Dictionary<Type, List<Component>>> mainEntry in _secondaryComponents)
-            {
-                Debug.Log("The main type: " + mainEntry.Key);
-                foreach (KeyValuePair<Type, List<Component>> secondaryEntry in mainEntry.Value)
-                {
-                    Debug.Log("Has " + secondaryEntry.Value.Count + " entries with secondary type " + secondaryEntry.Key);
-                }
-            }
-            */
+        void RemoveSecondaryComponent(Type mainComponentType, Component secondaryComponent)
+        {
+            if (!_secondaryComponents.ContainsKey(mainComponentType))
+                return;
+            Type secondaryComponentType = secondaryComponent.GetType();
+            if (!_secondaryComponents[mainComponentType].ContainsKey(secondaryComponentType))
+                return;
+            _secondaryComponents[mainComponentType][secondaryComponentType].Remove(secondaryComponent);
+            if (_secondaryComponents[mainComponentType][secondaryComponentType].Count == 0)
+                _secondaryComponents[mainComponentType].Remove(secondaryComponentType);
+            if (_secondaryComponents[mainComponentType].Count == 0)
+                _secondaryComponents.Remove(mainComponentType);
         }
 
         void RegisterFactoryHandlers()
